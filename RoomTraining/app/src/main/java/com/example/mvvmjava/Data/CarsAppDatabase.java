85754454ffb6268eba627fd8668ac76c51c901c1
StuThEmp <semenshulga1@gmail.com>
2021-04-12 15:39:29 +0300

    room added

---
+++ RoomTraining/app/src/main/java/com/example/mvvmjava/Data/CarsAppDatabase.java
@@ -0,0 +1,12 @@
+package com.example.mvvmjava.Data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.mvvmjava.Model.Car;
+
+@Database(entities = {Car.class}, version = 1)
+public abstract class CarsAppDatabase extends RoomDatabase {
+
+    public abstract CarDAO getCarDAO();
+}
